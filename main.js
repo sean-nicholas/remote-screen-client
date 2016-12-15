5d8d8e087a97401b97be8d07331ca5ac2a86cd0e
Sean Nicholas Dieterle <n.dieterle@netvise.de>
2016-12-15 20:18:43 +0100

    no menu bar

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.setMenu(null)
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
